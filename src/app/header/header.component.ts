9b90929b1a4cce9c878491e1979952d3b9555875
viacheslav.plesko <viacheslav.plesko@pit.lukasiewicz.gov.pl>
2025-10-31 14:22:15 +0100

    Initial commit: Task Manager Angular application

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  // imports: [UserComponent],
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
+})
+
+export class HeaderComponent {
+[x: string]: any;
+}
