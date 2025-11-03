fec9ab60ca547e120e567925a992c411d4313b9b
viacheslav.plesko <viacheslav.plesko@pit.lukasiewicz.gov.pl>
2025-11-03 15:38:59 +0100

    tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
+
+
 export class TasksComponent {
+  @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTask: boolean = false;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Task 1',
+      dueDate: '2025-01-01',
+      summary: 'Summary 1'
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string){
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = {
-//   id: string, 
-//   avatar: string, 
-//   name: string
-// };
-interface User {
-  id: string, 
-  avatar: string, 
-  name: string
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -21,6 +11,7 @@ interface User {
 
 export class UserComponent {
   @Input({required: true}) user!: User;
+  @Input({required: true}) isSelected!: boolean;
   @Output() select = new EventEmitter<string>();
   
   get imagePath() {
